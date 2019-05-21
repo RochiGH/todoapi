1a40f336eb50bb1b6250985cf78daa7ddf29f7a8
Rocco Ferrante <rferrante@avmap.it>
2019-05-21 09:44:21 +0200

    git dev branch init

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                //_context.TodoItems.Add(new TodoItem { Id = 1 });
+                _context.SaveChanges();
+
+            }
+        }
+
+        // GET api/todo/fuck 
+        [HttpGet("fuck")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "fuck1", "fuck2" };
+        }
+
+        // GET api/todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET api/todo/id like api/todo/5
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            else
+                return todoItem;
+        }
+
+    }
+}
---
+++ Models/DoneContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class DoneContext : DbContext
+    {
+        public DoneContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DoneItem> DoneItems { get; set; }
+    }
+}
